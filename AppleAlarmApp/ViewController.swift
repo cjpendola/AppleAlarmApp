5ec21f469d0d95baa63b0e246dd72e366d333828
Carlos Pendola <cjpendola@hotmail.com>
2019-03-07 10:38:07 -0700

    initial commit

---
+++ AppleAlarmApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppleAlarmApp
+//
+//  Created by Carlos Pendola on 3/7/19.
+//  Copyright © 2019 Carlos Pendola. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
