5f7f5fe9d53b0a6d3fdb4a834f1c546f5159f2df
RYAN GREENBURG <ryangreenburg@RYANs-MacBook-Pro.local>
2019-03-07 15:38:14 -0700

    Adds completed ryan views

---
+++ AppleAlarmApp/AlarmViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AlarmViewController.swift
+//  AppleAlarmApp
+//
+//  Created by RYAN GREENBURG on 3/7/19.
+//  Copyright © 2019 Carlos Pendola. All rights reserved.
+//
+
+import UIKit
+
+class AlarmViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
